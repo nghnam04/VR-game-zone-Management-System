a48f7be76c6194db33c0d96a7eee1005b119f978
Nguyen Hoang Nam <hoangnam2004hp@gmail.com>
2025-11-13 15:59:42 +0700

    fix: update CORS to allow deployed frontend & set API_URL to deployed backend

--- backend/src/main/java/vn/edu/hust/vrgamesapp/config/SecurityConfig.java
+++ backend/src/main/java/vn/edu/hust/vrgamesapp/config/SecurityConfig.java
@@ -66,7 +66,7 @@ public class SecurityConfig {
     @Bean
     public CorsConfigurationSource corsConfigurationSource() {
         CorsConfiguration configuration = new CorsConfiguration();
-        configuration.setAllowedOrigins(Arrays.asList("http://localhost:5173"));
+        configuration.setAllowedOrigins(Arrays.asList("http://localhost:5173", "https://vrealm-game-zone.netlify.app"));
         configuration.setAllowedMethods(Arrays.asList("GET", "POST", "PUT", "PATCH", "DELETE", "OPTIONS"));
         configuration.setAllowedHeaders(Arrays.asList("*"));
         configuration.setAllowCredentials(true);
